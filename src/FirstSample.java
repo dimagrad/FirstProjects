001a8f3d74b1a2b418b64a925dd0c207918adf0b
dimagrad <dima.belyj2012@gmail.com>
2020-10-08 02:34:05 +0300

    first commit

---
+++ src/FirstSample.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class FirstSample {
+    public static void main (String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Write something");
+        String stroka = in.nextLine();
+        System.out.println(stroka);
+    }
+}
